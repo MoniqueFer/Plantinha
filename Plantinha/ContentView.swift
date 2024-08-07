705f5a2eb628bde71c2011035e92fab42f140433
MoniqueFer <138914572+MoniqueFer@users.noreply.github.com>
2024-08-07 15:11:40 -0300

    Initial Commit

---
+++ Plantinha/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Plantinha
+//
+//  Created by Monique Ferrarini on 07/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Plantinha/PlantinhaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantinhaApp.swift
+//  Plantinha
+//
+//  Created by Monique Ferrarini on 07/08/24.
+//
+
+import SwiftUI
+
+@main
+struct PlantinhaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
